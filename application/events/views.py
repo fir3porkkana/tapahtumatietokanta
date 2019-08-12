7764e94c9a2558f0384e2f92be6c64b9c7fa9eb1
fir3porkkana <tiera.salmi@gmail.com>
2019-08-12 16:50:58 +0300

    crud toiminnassa, kirjautumista vaaditaan joidenkin routejen kohdalla

--- application/events/views.py
+++ application/events/views.py
@@ -1,7 +1,8 @@
 from application import app, db
 from flask import render_template, request, url_for, redirect
+from flask_login import login_required
 from application.events.models import Event
-from application.events.forms import EventForm
+from application.events.forms import EventForm, ModifyForm
 
 @app.route("/events/", methods=["GET"])
 def events_index():
@@ -9,26 +10,46 @@ def events_index():
 
 
 @app.route("/events/new/")
+@login_required
 def events_form():
     return render_template("events/new.html", form = EventForm())
 
+@app.route("/events/delete/<event_id>", methods=["POST"])
+@login_required
+def events_delete(event_id):
+    e = Event.query.get(event_id)
+    db.session.delete(e)
+    db.session.commit()
+    return redirect(url_for("events_index"))
+
 
 @app.route("/events/<event_id>/", methods=["POST"])
+@login_required
 def events_set_cancelled(event_id):
+    name = request.form.get("name")
+    desc = request.form.get("description")
     
-    e = Event.query.get(event_id)
-    e.cancelled = False if e.cancelled else True
-    db.session().commit()
 
-    return redirect(url_for("events_index"))
+    e = Event.query.get(event_id)
+    if name:
+        e.name = name
+    elif desc:
+        e.description = desc
+    else:
+        e.cancelled = False if e.cancelled else True
+    
+    db.session.commit()
+
+    return render_template("events/one.html", event = e, form = ModifyForm())
 
 @app.route("/events/<event_id>/", methods=["GET"])
 def event_view_by_id(event_id):
     e = Event.query.get(event_id)
 
-    return render_template("events/one.html", event = e, form = EventForm())
+    return render_template("events/one.html", event = e, form = ModifyForm())
 
 @app.route("/events/", methods=["POST"])
+@login_required
 def events_create():
     print(request.form.get("name"))
     print(request.form.get("description"))
@@ -45,6 +66,7 @@ def events_create():
     return redirect(url_for("events_index"))
 
 @app.route("/events/modify/<event_id>", methods=["POST"])
+@login_required
 def events_modify(event_id):
     
     return render_template("events/one.html", event = e)
